e4b6397e1d6518ac5dd851475ca0f48d495b82ad
Matt Molinar <matthew.a.molinar@gmail.com>
2023-02-24 22:56:32 -0600

    Initial Commit

---
+++ Molinar/MolinarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MolinarApp.swift
+//  Molinar
+//
+//  Created by Matt Molinar on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MolinarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
