c1483cfea370a3282da70db10a786d03ad18c513
Matt Molinar <matthew.a.molinar@gmail.com>
2023-03-04 19:14:54 -0600

    3d location puck

--- Molinar/Core/Home/Views/Map/MapboxView.swift
+++ Molinar/Core/Home/Views/Map/MapboxView.swift
@@ -62,8 +62,48 @@ class MapboxView: UIViewController, CLLocationManagerDelegate {
 //        pointAnnotation.iconAnchor = .bottom
 //        let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
 //        pointAnnotationManager.annotations = [pointAnnotation]
+        // Fetch the `gltf` asset
+        let uri = Bundle.main.url(forResource: "locationPuck",
+                                         withExtension: "glb")
+        let locationModel = Model(uri: uri, orientation: [0, 0, 180])
+        // Setting an expression to  scale the model based on camera zoom
+               let scalingExpression = Exp(.interpolate) {
+                   Exp(.linear)
+                   Exp(.zoom)
+                   0
+                   Exp(.literal) {
+                       [256000.0, 256000.0, 256000.0]
+                   }
+                   4
+                   Exp(.literal) {
+                       [40000.0, 40000.0, 40000.0]
+                   }
+                   8
+                   Exp(.literal) {
+                       [2000.0, 2000.0, 2000.0]
+                   }
+                   12
+                   Exp(.literal) {
+                       [100.0, 100.0, 100.0]
+                   }
+                   16
+                   Exp(.literal) {
+                       [7.0, 7.0, 7.0]
+                   }
+                   20
+                   Exp(.literal) {
+                       [1.0, 1.0, 1.0]
+                   }
+               }
+        let configuration = Puck3DConfiguration(model: locationModel, modelScale: .expression(scalingExpression), modelOpacity: .constant(1))
+        mapView.location.options.puckType = .puck3D(configuration)
+        mapView.location.options.puckBearingSource = .course
+
+
+
+
         
-        mapView.location.options.puckType = .puck2D()
+        
 
         self.view.addSubview(mapView)
         
