48499f02ca9ccf08687a35ea91aaccf05f67e17d
Matt Molinar <matthew.a.molinar@gmail.com>
2023-03-05 17:12:48 -0600

    added coordinate to Task Object Model

--- Molinar/Models/Task.swift
+++ Molinar/Models/Task.swift
@@ -6,6 +6,7 @@
 //
 
 import Firebase
+import CoreLocation
 
 
 struct Task: Identifiable {
@@ -17,6 +18,7 @@ struct Task: Identifiable {
     let title: String
     let duration: Int
     let priority: String
+    let coordinate: CLLocationCoordinate2D
     
     
     
@@ -30,5 +32,6 @@ struct Task: Identifiable {
         self.title = dictionary["title"] as? String ?? ""
         self.priority = dictionary["priority"] as? String ?? ""
         self.duration = dictionary["duration"] as? Int ?? 1800 // 30 mins in seconds
+        self.coordinate = dictionary["coordinate"] as? CLLocationCoordinate2D ?? CLLocationCoordinate2D(latitude: 32.8226283, longitude: -96.8254078)
     }
 }
