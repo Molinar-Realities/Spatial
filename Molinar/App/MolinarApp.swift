d50a0be58099b7db85cdc3b8ba54d81023f13562
Matt Molinar <matthew.a.molinar@gmail.com>
2023-03-07 15:18:27 -0600

    getting permission for notifications

--- Molinar/App/MolinarApp.swift
+++ Molinar/App/MolinarApp.swift
@@ -7,15 +7,30 @@
 
 import SwiftUI
 import FirebaseCore
+import UserNotifications
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        // Request permission for local notifications
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if let error = error {
+                // Handle the error
+                print("Error requesting permission for notifications: \(error.localizedDescription)")
+            } else if granted {
+                // Authorization granted
+                print("Permission granted for notifications")
+            } else {
+                // Authorization denied
+                print("Permission denied for notifications")
+            }
+        }
+        
+        return true
+    }
 }
 
 @main
