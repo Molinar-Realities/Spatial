74a65b08f16e914c0d4e61281616930b45236dcb
Matt Molinar <matthew.a.molinar@gmail.com>
2023-03-06 19:49:46 -0600

    TaskList

--- Molinar/Core/Home/Views/BottomSheet/Screens/FocusNow/TaskCell.swift
+++ Molinar/Core/Home/Views/BottomSheet/Screens/FocusNow/TaskCell.swift
@@ -15,38 +15,41 @@ struct TaskCell: View {
     @State var dueDate = Date()
     
     var body: some View {
-        HStack {
-            Button(action: {
-                let impact = UIImpactFeedbackGenerator(style: .medium)
-                impact.impactOccurred()
-                
-                DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 3) {
-                                   removed.toggle()
-                               }
-                            withAnimation {
-                                completed.toggle()
-                                self.isAnimatingCheckmark = true
-                            }
-                        }) {
-                            Image(systemName: completed ? "checkmark.circle.fill" : "circle")
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                                .foregroundColor(completed ? .green : .primary)
-                                .rotationEffect(Angle(degrees: self.isAnimatingCheckmark ? 360 : 0))
-                                .animation(.easeInOut(duration: 0.3))
-                                .onAppear {
-                                    self.isAnimatingCheckmark = false
+        VStack {
+            HStack {
+                Button(action: {
+                    let impact = UIImpactFeedbackGenerator(style: .medium)
+                    impact.impactOccurred()
+                    
+                    DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 3) {
+                                       removed.toggle()
+                                   }
+                                withAnimation {
+                                    completed.toggle()
+                                    self.isAnimatingCheckmark = true
                                 }
-                        }
-                        .opacity(removed ? 0 : 1)
-                        .animation(.easeInOut(duration: 0.3))
-            Text("title")
-                                .opacity(removed ? 0 : 1)
-                                .animation(.easeInOut(duration: 0.3))
-            Spacer()
-            Text(dueDate.isToday ? dueDate.toString(dateFormat: "h:mm a") : dueDate.toString(dateFormat: "MMM d"))
-                .opacity(removed ? 0 : 1)
-                .animation(.easeInOut(duration: 0.3))
+                            }) {
+                                Image(systemName: completed ? "checkmark.circle.fill" : "circle")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                    .foregroundColor(completed ? .green : .primary)
+                                    .rotationEffect(Angle(degrees: self.isAnimatingCheckmark ? 360 : 0))
+                                    .animation(.easeInOut(duration: 0.3))
+                                    .onAppear {
+                                        self.isAnimatingCheckmark = false
+                                    }
+                            }
+                            .opacity(removed ? 0 : 1)
+                            .animation(.easeInOut(duration: 0.3))
+                Text("title")
+                                    .opacity(removed ? 0 : 1)
+                                    .animation(.easeInOut(duration: 0.3))
+                Spacer()
+                Text(dueDate.isToday ? dueDate.toString(dateFormat: "h:mm a") : dueDate.toString(dateFormat: "MMM d"))
+                    .opacity(removed ? 0 : 1)
+                    .animation(.easeInOut(duration: 0.3))
+            }
+            Divider()
         }
     }
 }
--- Molinar/Core/Home/Views/BottomSheet/Screens/FocusNow/TasksList.swift
+++ Molinar/Core/Home/Views/BottomSheet/Screens/FocusNow/TasksList.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct TasksList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ForEach(0..<10) { _ in
+                TaskCell()
+                    .padding()
+            }
+        }
     }
 }
 
